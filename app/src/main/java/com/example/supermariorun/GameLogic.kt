9cce94c41f532f4ea2eeeca964efcc52e18a5a45
Tura2 <offir.tura@gmail.com>
2025-04-24 16:58:46 +0300

    minor fix

--- app/src/main/java/com/example/supermariorun/GameLogic.kt
+++ app/src/main/java/com/example/supermariorun/GameLogic.kt
@@ -2,8 +2,6 @@ package com.example.supermariorun
 
 import android.content.Context
 import android.os.CountDownTimer
-import android.os.Handler
-import android.os.Looper
 import android.util.Log
 import android.widget.ImageView
 import com.example.supermariorun.Utilities.SignalManager
@@ -60,7 +58,6 @@ class GameLogic(
             override fun onTick(millisUntilFinished: Long) {
                 val row = currentRow
 
-                // Clear previous shell cell
                 if (row > 0) {
                     cellMatrix[row - 1][col].setImageDrawable(null)
                     cellMatrix[row - 1][col].tag = null
@@ -68,11 +65,11 @@ class GameLogic(
 
                 if (row >= numRows) return
 
-                // Draw the shell and set tag
+
                 cellMatrix[row][col].setImageResource(drawableId)
                 cellMatrix[row][col].tag = "shell"
 
-                // Hit from above
+
                 if (row == numRows - 1 && col == lane) {
                     loseLife()
                 }
@@ -81,7 +78,7 @@ class GameLogic(
             }
 
             override fun onFinish() {
-                // Clear shell if still there
+
                 if (currentRow == numRows && col != lane) {
                     cellMatrix[numRows - 1][col].setImageDrawable(null)
                     cellMatrix[numRows - 1][col].tag = null
